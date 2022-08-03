2623d816a6703e2746aac3b4ac4cf4a767a9e65c
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2022-08-02 20:21:15 -0400

    Update user_signup.php

--- user_signup.php
+++ user_signup.php
@@ -1,32 +1,26 @@
 <?php
-session_start();
-$firstName= $_POST["first_name"];
+ require_once('connection.php');
+ 
+$firstName= $_POST['first_name'];
 $lastName  = $_POST["last_name"];
 $email = $_POST["email"];
 $pass_wordd = $_POST["pass_word"];
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "cooking_robot_db";
-
-
-$conn = mysqli_connect($host,$username,$password,$dbname) or die("Couldn't connect to");
 $check="SELECT * FROM users_table WHERE email = '$email'";
-echo $check;
 $result = mysqli_query($conn,$check);
+
 if(mysqli_num_rows($result)>0)
 {
   $_SESSION['error'] = "Error";
   header("Location:register.php");
-
+echo "error";
 }
 
 else{
-$sql = "INSERT INTO users_table VALUES ('$email','$firstName', '$lastName', '$pass_wordd')";
-$res = mysqli_query($conn,$sql);
-echo $res;
-if($res)
-  header("Location:login.php");
+$sql = "INSERT INTO users_table (email, first_name, last_name,pass_word) VALUES ('$email','$firstName', '$lastName', '$pass_wordd')";
+$res = mysqli_query($conn,$sql);  
+
+header("Location:login.php");
 }
+
 ?>
