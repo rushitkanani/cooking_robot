0c9b0bdb33801b27d3a75f5974dab26b403e6c58
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2022-08-04 20:42:16 -0400

    Update ingredients.php

--- ingredients.php
+++ ingredients.php
@@ -28,31 +28,31 @@ else{
               <div class="row">
                 <label class="col-sm-4 form-control-label">Ingredient Name: <span class="tx-danger">*</span></label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                  <input type="text" class="form-control" placeholder="Enter Ingredient Name" id="ingrident_name" name="ingrident_name" required>
+                  <input type="text" class="form-control" placeholder="Enter Ingredient Name" id="ingrident_name" name="ingrident_name" required oninvalid="this.setCustomValidity('Enter Ingredient Name')" oninput="this.setCustomValidity('')">
                 </div>
               </div><!-- row -->
               <div class="row mg-t-20">
-                <label class="col-sm-4 form-control-label">Cooking Time: <span class="tx-danger">*</span></label>
+                <label class="col-sm-4 form-control-label">Cooking Time: </label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                  <input type="text" class="form-control" placeholder="Enter Cooking Time" id="cooking_time" name="cooking_time"  required>
+                  <input type="text" class="form-control" placeholder="Enter Cooking Time" id="cooking_time" name="cooking_time"  >
                 </div>
               </div>
               <div class="row mg-t-20">
-                <label class="col-sm-4 form-control-label">Cooking Temperature: <span class="tx-danger">*</span></label>
+                <label class="col-sm-4 form-control-label">Cooking Temperature: </label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                  <input type="text" class="form-control" placeholder="Enter Cooking Temperature" id="cooking_temp" name="cooking_temp"  required>
+                  <input type="text" class="form-control" placeholder="Enter Cooking Temperature" id="cooking_temp" name="cooking_temp">
                 </div>
               </div>
               <div class="row mg-t-20">
                 <label class="col-sm-4 form-control-label">Calories: <span class="tx-danger">*</span></label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                <input type="text" class="form-control" placeholder="Enter Calories" id="calories" name="calories" required>
+                <input type="text" class="form-control" placeholder="Enter Calories" id="calories" name="calories" required oninvalid="this.setCustomValidity('Enter Calories')" oninput="this.setCustomValidity('')" >
                 </div>
               </div>
               <div class="row mg-t-20">
                 <label class="col-sm-4 form-control-label">Upload Image: <span class="tx-danger">*</span></label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                <input type="file" class="form-control" id="image" name="image" accept="image/*"/>
+                <input type="file" class="form-control" id="image" name="image" accept="image/*" required oninvalid="this.setCustomValidity('Upload Ingredient Image')" oninput="this.setCustomValidity('')">
                 </div>
               </div>
               
@@ -62,16 +62,16 @@ else{
             <!-- form-layout -->
             </div>
           </div><!-- col-6 -->
-</form>
+</form   >
           <div class="col-xl-6 mg-t-20 mg-xl-t-0" style="background-color: #fff;">
             <div class="form-layout form-layout-4">
               <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Add Utensils</h6>
               <p class="mg-b-30 tx-gray-600">Add your utensils here</p>
-              <form>
+              <form action="add_utensil.php" method= "GET" >
               <div class="row">
-                <label class="col-sm-4 form-control-label"><span class="tx-danger">*</span> Utensil Name:</label>
+                <label class="col-sm-4 form-control-label"> Utensil Name:<span class="tx-danger">*</span></label>
                 <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                <input type="text" id="utensil" name="utensil"   required>
+                <input type="text" id="utensil" name="utensil" required oninvalid="this.setCustomValidity('Enter Utensil Name')" oninput="this.setCustomValidity('')">
 </div>
                 <div class="form-layout-footer mg-t-30" style="padding-left: 35%;">
                 <button class="btn btn-info btn-dark active" id="add_utensil" name="add_utensil">Add Utensil</button>
@@ -82,7 +82,7 @@ else{
             </div><!-- form-layout -->
           </div><!-- col-6 -->
 </form>
-        <script src="ajax.js"></script>
+        <!-- <script src="ajax.js"></script>
 <script type = "text/javascript">
   console.log("hello")
   $(function () {
@@ -91,7 +91,7 @@ else{
         console.log("clicked!!");
         var utensil  = $('#utensil').val();
         $.ajax({
-          type: 'post',
+          type: 'POST',
           url: 'add_utensil.php',
           dataType: 'json',
           data: {
@@ -105,6 +105,6 @@ else{
       })
       });
 
-</script>
+</script> -->
 </body>
 </html>
