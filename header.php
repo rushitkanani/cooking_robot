fe2cd4bb5600f3deb79375c235ec781e4f817a29
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2022-08-05 15:22:21 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,52 +1,21 @@
-
-
-<header>
-<link rel="stylesheet" href="css/grids.css">
-        <img class="logo" src="images/logo.png">
-        <h2>Cooking Robot</h2>
-
-        <nav>
-            <ul>
-                <li>
-                    <a href="homepage.php">Home</a>
-                </li>
-                <li>
-                    <a href="#">About Us</a>
-
-                </li>
-                <li>
-                    <a target="_blank" href="recipes.html">Recipes</a>
-                    <ul>
-                        <li>
-                            <a href="##images4">Chinese</a>
-                        </li>
-                        <li>
-                            <a href="allgrids.html#mycards">Gujarati</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Punjabi</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Pizza</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Turkey</a>
-                        </li>
-                    </ul>
-                </li>
-                
-                <li>
-                            <a href="#">Welcome Back <?php echo $_SESSION['name']; ?></a>
-                
-                            <ul>
-                                <li>
-                                    <a href="user_profile.php">My Profile</a>
-                                </li>
-                                <li>
-                                    <a href="user_logout.php">Logout</a>
-                                </li>
-                            </ul>
-                </li>
-
-        </nav>
-    </header>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cooking Robot</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+    <body>
+    <div class="pd-10 mg-t-20 bg-dark rounded">
+      <div class="ht-65 bd pd-x-20 d-flex align-items-center justify-content-between">
+      <h4 class="mg-b-0 tx-bold tx-spacing--2 tx-inverse tx-poppins mg-r-20"><span style="color: white; font-size: 35px;">Cooking</span><span style="color: rgb(6, 193, 103); font-size: 35px;"> Robot</span></h4>
+        <ul class="nav nav-white-800 flex-column flex-md-row justify-content-end" role="tablist">
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" data-toggle="tab" href="#" role="tab">Home</a></li>
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" data-toggle="tab" href="#" role="tab">About Us</a></li>
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" href="login.php">Login / Sign Up</a></li>
+        </ul>
+      </div>
+      </div>
+      
+      
+</body>
+</html>
