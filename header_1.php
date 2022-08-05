2cb81192898c67478ac37e05a1e4c0ab5aef4403
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2022-08-05 15:21:43 -0400

    Update header_1.php

--- header_1.php
+++ header_1.php
@@ -1,53 +1,19 @@
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cooking Robot</title>
-    <link rel="stylesheet" href="css/grids.css">
-</head>
-<body>
-    <header>
-        <img class="logo" src="images/logo.png">
-        <h2>Cooking Robot</h2>
-        <nav>
-            <ul>
-                <li>
-                    <a href="homepage.php">Home</a>
-                    <ul>
-                        <li>
-                            <a href="#">About Us</a>
-                        </li>
-                        <li>
-                            <a href="#">Recipes</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">About Us</a>
-                </li>
-                <li>
-                    <a target="_blank" href="recipes.html">Recipes</a>
-                    <ul>
-                        <li>
-                            <a href="##images4">Chinese</a>
-                        </li>
-                        <li>
-                            <a href="allgrids.html#mycards">Gujarati</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Punjabi</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Pizza</a>
-                        </li>
-                        <li>
-                            <a href="##images4">Turkey</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="login.php">Login/Signup</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+<link rel="stylesheet" href="css/grids.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+        <div class="pd-10 mg-t-20 bg-dark rounded">
+      <div class="ht-65 bd pd-x-20 d-flex align-items-center justify-content-between">
+      <h4 class="mg-b-0 tx-bold tx-spacing--2 tx-inverse tx-poppins mg-r-20"><span style="color: white; font-size: 35px;">Cooking</span><span style="color: rgb(6, 193, 103); font-size: 35px;"> Robot</span></h4>
+        <ul class="nav nav-white-800 flex-column flex-md-row justify-content-end" role="tablist">
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" href="homepage.php" role="tab">Home</a></li>
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" href="ingredient_list.php" role="tab">Ingredient List</a></li>
+          <li class="nav-item" style="font-size: 15px;"><a class="nav-link" href="homepage.php" role="tab">About Us</a></li>
+          <li class="nav-item dropdownn" style="font-size: 15px;"><a class="nav-link" href="#">Welcome Back, <?php echo $_SESSION['name']; ?></a>
+          <div class="dropdown-content">
+
+    <a href="user_profile.php">My Profile</a>
+    <a href="user_logout.php">Logout</a>
+  </div>
+  </li></ul>
+      </div>
+      </div>
