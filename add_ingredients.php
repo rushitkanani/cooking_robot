a72552f3185910dee43ac2ecbd8ae42c8eeba813
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2022-08-13 22:47:56 -0400

    Update add_ingredients.php

--- add_ingredients.php
+++ add_ingredients.php
@@ -1,6 +1,5 @@
 <?php 
 require_once('connection.php');
-
 if(isset($_POST['upload']))
 {
     if($_FILES['image']['size'] > 50000000){
@@ -12,19 +11,31 @@ if(isset($_POST['upload']))
     $cooking_temp = $_POST['cooking_temp'];
     $calories = $_POST['calories'];
 
-    $query = "INSERT INTO `ingredient_table`(`ingredient_name`,`ingredient_calories`,`ingredient_time`,`ingredient_temperature`,`ingredient_photo`) values('$ingrident_name','$calories','$cooking_time','$cooking_temp','$file')";
-    $query_run = mysqli_query($conn, $query);
+    $sql = "SELECT * FROM `ingredient_table` WHERE `ingredient_name`='$ingrident_name'";
+    $res = mysqli_query($conn, $sql) or die("Error in SQL");
+    if(mysqli_num_rows($res) == 0){
+        $query = "INSERT INTO 
+        `ingredient_table`(`ingredient_name`,`ingredient_calories`,`ingredient_time`,`ingredient_temperature`,`ingredient_photo`) 
+        values('$ingrident_name','$calories','$cooking_time','$cooking_temp','$file')";
+        $query_run = mysqli_query($conn, $query);
 
-    if($query_run)
-    {
-        echo "<script> confirm('Ingredient Added Successfully');
-        window.location.assign('ingredients.php');
-        </script>";
-        
+        if($query_run)
+        {
+            echo "<script> confirm('Ingredient Added Successfully');
+            window.location.assign('ingredients.php');
+            </script>";    
+        }
+        else{
+            echo "<script> confirm('Something went wrong!!!');
+            window.location.assign('ingredients.php');
+            </script>";
+        }
+    }else{
+        echo "<script> confirm('IngredientAlready Exists!!!');
+            window.location.assign('ingredients.php');
+            </script>";
     }
-    else{
-        echo mysqli_error($conn);
-        echo "failed";
-    }
+
 }
 ?>
+
